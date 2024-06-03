bed5234418bdab375af1b1055d036239fe1ef61f
Adam Griffith <128840129+adamgriffith29@users.noreply.github.com>
2024-06-03 17:23:04 -0500

    Add files via upload

---
+++ EvenOdd.js
@@ -0,0 +1,8 @@
+displayNumbers(10);
+
+function displayNumbers(limit) {
+    for (let i =0 ; i<=limit; i++) {
+        const message = (i % 2 === 0) ? "EVEN" : "ODD";
+        console.log(i, message);
+    }
+}
